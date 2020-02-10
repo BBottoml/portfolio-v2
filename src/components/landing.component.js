853f5364acab1ce7c652bb0d39018be5619c99b6
Bradley Bottomlee <bradley.bttm@gmail.com>
2020-02-10 13:04:45 -0500

    Working commit

--- src/components/landing.component.js
+++ src/components/landing.component.js
@@ -5,7 +5,7 @@ import { fadeIn } from 'react-animations';
 import Radium, {StyleRoot} from 'radium';
 
 const styles = {
-    fadeInDown: {
+    fadeIn: {
       animation: 'x 1s',
       animationName: Radium.keyframes(fadeIn, 'fadeIn')
     }
@@ -18,8 +18,8 @@ export default class Landing extends Component {
         return(
             <div className="moduleIntro">
                 <StyleRoot>
-                    <div style={styles.fadeInDown}>
-                        <div className='text-center' style={{color: "#f5f6fa", paddingTop: "15%"}}>
+                    <div style={styles.fadeIn}>
+                        <div className='text-center' style={{paddingTop: "15%"}}>
                             <h1 style={{fontWeight: "150", fontSize: "60px"}}>Bradley Bottomlee</h1>
                             <p style={{fontWeight: "150", fontSize: "20px"}}>Computer Science student at Purdue University</p>
                             <div className='container'>
@@ -27,15 +27,15 @@ export default class Landing extends Component {
                             </div>
                         </div>
 
-                        <div className='container' style={{color: "#f5f6fa"}}>
+                        <div className='container'>
                             <p align="justify" style={{fontSize: "20px"}}>
-                            Hello, and welcome to my portfolio! My name is Bradley, and I'm a second-year, 
-                            computer science student at Purdue University. I'm passionate about developing super well-designed, 
-                            equitable software that is accessible to everyone regardless of technological ability. To this end, 
-                            I have sought to create <Link to="/projects">numerous projects</Link> to assist those in their respective pursuits. 
-                            My specific interests include distributed systems, web application development, and machine learning. Aside
-                            from software development, I'm also passionate about film, television, and reading. View my favorite films 
-                            on my <a href="https://letterboxd.com/bbottoml/">Letterboxd account</a>!
+                                Hello, and welcome to my portfolio! My name is Bradley, and I'm a second-year, 
+                                computer science student at Purdue University. I'm passionate about developing super well-designed, 
+                                equitable software that is accessible to everyone regardless of technological ability. To this end, 
+                                I have sought to create <Link to="/projects">numerous projects</Link> to assist those in their respective pursuits. 
+                                My specific interests include distributed systems, web application development, and machine learning. Aside
+                                from software development, I'm also passionate about film, television, and reading. View my favorite films 
+                                on my <a href="https://letterboxd.com/bbottoml/">Letterboxd account</a>!
                             </p>
                             <div className='text-center' style={{fontSize: "20px"}}>
                                 <a href="https://twitter.com/bottomlee_b"><i class="fab fa-twitter"></i></a> &nbsp; 
--- src/components/projects.component.js
+++ src/components/projects.component.js
@@ -1,12 +1,81 @@
 import React, { Component } from 'react'
-import './sidebar.css'
+import { Link } from 'react-router-dom'
+import { fadeIn, fadeInUp, fadeInDown } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
+import { Row, Image, Col} from 'react-bootstrap'
+// import images
+import beatq from '../images/beatq.png'
+import './landing.css'
+// http://seanhalpin.io/work
+const styles = {
+    fadeIn: {
+      animation: 'x 1s',
+      animationName: Radium.keyframes(fadeIn, 'fadeIn')
+    }, 
+    fadeInUp: {
+        animation: 'x 1.5s',
+        animationName: Radium.keyframes(fadeInUp, 'fadeInUp')
+    }, 
+    fadeInDown: {
+        animation: 'x 1s', 
+        animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+    }
+  }
 
 export default class Projects extends Component {
 
     render() {
         return(
-            <div className='text-center'>
-                <h1>Hello world</h1>
+            <div>
+                <StyleRoot>
+                        <div className='text-center' style={{paddingTop: "5%"}}>
+                            <div style={styles.fadeInDown}>
+                                <h1 style={{fontWeight: "150", fontSize: "60px"}}>Projects</h1>
+                            </div>
+                            <div className='container' style={styles.fadeIn}>
+                                <hr className='landing' />
+                            </div>
+                        </div>
+                        {/* BeatQ Project */}
+                        <div style={styles.fadeInUp}>
+                            <div style={{paddingLeft: "5%", paddingRight: "5%"}}>
+                            <Row>
+                                <Col>
+                                    <Image src={beatq} thumbnail style={{marginRight: "2%"}} />
+                                </Col>
+                                <Col>
+                                    <h2 style={{fontWeight: "150", fontSize: "40px"}}>BeatQ - DemonHacks 2nd place winner</h2>
+                                    <p align='justify' style={{fontWeight: "150", fontSize: "20px"}}>
+                                        BeatQ is a web application that allows groups to seamlessly queue music by searching for their
+                                        desired songs and adding it to the party queue. I contributed to this project as part of a four person 
+                                        team at the DemonHacks hacakthon at DePaul University. The tech stack used included Flask, Python, 
+                                        Bootstrap, HTML/CSS. I worked on the implementation of cookies, Flask routes, 
+                                        overarching logic, front-end design such as the dashboard, and integration of Spotify API in coordination
+                                        with teammates. 
+                                    </p>
+                                </Col>
+                            </Row>
+                            </div>
+                            <Row>
+                                <Col>
+                                    <Image src={beatq} thumbnail style={{marginRight: "2%"}} />
+                                </Col>
+                                <Col>
+                                    <h2 style={{fontWeight: "150", fontSize: "40px"}}>BeatQ - DemonHacks 2nd place winner</h2>
+                                    <p align='justify' style={{fontWeight: "150", fontSize: "20px"}}>
+                                        BeatQ is a web application that allows groups to seamlessly queue music by searching for their
+                                        desired songs and adding it to the party queue. I contributed to this project as part of a four person 
+                                        team at the DemonHacks hacakthon at DePaul University. The tech stack used included Flask, Python, 
+                                        Bootstrap, HTML/CSS. I worked on the implementation of cookies, Flask routes, 
+                                        overarching logic, front-end design such as the dashboard, and integration of Spotify API in coordination
+                                        with teammates. 
+                                    </p>
+                                </Col>
+                            </Row>
+
+
+                            </div>
+                </StyleRoot>
             </div>
         )
     }
